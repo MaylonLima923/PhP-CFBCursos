3db9b466d5b70cc6f50659f3a1f498126c6c7557
Maylon Lima <maylonlima923@gmail.com>
2021-09-19 13:23:09 -0300

    Finalizando curso

--- conecta.php
+++ conecta.php
@@ -1,6 +1,6 @@
 <?php
     // Cria a conexão
-    $conn = mysqli_connect("localhost:3306", "Maylon", "May%190233");
+    $conn = mysqli_connect("localhost:3306", "usuario", "senha");
     // Usa a database
     mysqli_select_db($conn, "canalcfb");
 ?>
